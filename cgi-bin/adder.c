84dbebf59f548233cef5b609843b03b07579e72b
zeroistfilm <zeroistfilm@naver.com>
2021-01-25 16:42:18 +0900

    add adder

--- cgi-bin/adder.c
+++ cgi-bin/adder.c
@@ -5,18 +5,31 @@ int main(void) {
     char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
     int n1 = 0, n2 = 0;
 
+    sprintf(content, "Welcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+
     if ((buf = getenv("QUERY_STRING")) != NULL) {
+//        sprintf(content, "%s argv Slising \r\n<p>", content);
+
         p = strchr(buf, '&');
         *p = '\0';
         strcpy(arg1, buf);
         strcpy(arg2, p+1);
-        printf("222\n");
+
+        p = strchr(arg1, '=');
+        *p = '\0';
+        strcpy(arg1, p+1);
+
+        p = strchr(arg2, '=');
+        *p = '\0';
+        strcpy(arg2, p+1);
+
+
         n1 = atoi(arg1);
         n2 = atoi(arg2);
     }
-    printf("111\n");
-    sprintf(content, "asdada Welcome to add.com: ");
-    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+
+//    sprintf(content, "%s n1 %s n2 %s \r\n<p>", content, arg1, arg2);
     sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1+n2);
     sprintf(content, "%sThanks for visiting!\r\n", content);
 
--- csapp.c
+++ csapp.c
@@ -778,15 +778,16 @@ ssize_t rio_writen(int fd, void *usrbuf, size_t n)
     ssize_t nwritten;
     char *bufp = usrbuf;
 
-    while (nleft > 0) {
-	if ((nwritten = write(fd, bufp, nleft)) <= 0) {
-	    if (errno == EINTR)  /* Interrupted by sig handler return */
-		nwritten = 0;    /* and call write() again */
-	    else
-		return -1;       /* errno set by write() */
-	}
-	nleft -= nwritten;
-	bufp += nwritten;
+    if (nleft > 0) {
+        if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+            if (errno == EINTR)  /* Interrupted by sig handler return */
+                nwritten = 0;    /* and call write() again */
+            else
+                return -1;       /* errno set by write() */
+        }
+        nleft -= nwritten;
+        printf("nleft  : %ld, nwritten %ld \n", nleft, nwritten);
+        bufp += nwritten;
     }
     return n;
 }
